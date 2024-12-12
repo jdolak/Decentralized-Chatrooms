d31b900911322670343c9682667b46214bf693ee
Jachob Dolak <dolak.jachob@gmail.com>
2024-12-11 23:49:53 -0500

    condor

--- submit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tar -xzf ./src.tar
-tar -xzf ./ppy.tar
-
-source ./ppy/bin/activate
-
-./ppy/bin/python3.12 ./src/main.py --headless=True --username=test-join-$HOSTNAME --join=student10.cse.nd.edu:37023
